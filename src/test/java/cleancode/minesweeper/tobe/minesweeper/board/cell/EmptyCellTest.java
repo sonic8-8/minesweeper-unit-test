0da94a320349e78f74639d86bcb9e2df9394038c
이승환 <seaung13@gmail.com>
2025-06-22 08:28:28 +0900

    test: [minesweeper] EmptyCell, LandMineCell, NumberCell.getSnapshot() 조건별 반환 테스트 추가

--- src/test/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCellTest.java
+++ src/test/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCellTest.java
@@ -1,9 +1,56 @@
 package cleancode.minesweeper.tobe.minesweeper.board.cell;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class EmptyCellTest {
 
+    @DisplayName("사용자가 빈 칸을 열면, EMPTY 상태의 스냅샷을 반환한다.")
+    @Test
+    void getSnapshotWithOpenedEmptyCell() {
+        // given
+        EmptyCell emptyCell = new EmptyCell();
+        emptyCell.open();
 
+        // when
+        CellSnapshot snapshot = emptyCell.getSnapshot();
 
+        // then
+        assertThat(snapshot)
+                .extracting("status", "nearbyLandMineCount")
+                .containsExactlyInAnyOrder(CellSnapshotStatus.EMPTY, 0);
+    }
+
+    @DisplayName("사용자가 빈 칸에 깃발을 꽂으면, FLAG 상태의 스냅샷을 반환한다.")
+    @Test
+    void getSnapshotWithFlaggedEmptyCell() {
+        // given
+        EmptyCell emptyCell = new EmptyCell();
+        emptyCell.flag();
+
+        // when
+        CellSnapshot snapshot = emptyCell.getSnapshot();
+
+        // then
+        assertThat(snapshot)
+                .extracting("status", "nearbyLandMineCount")
+                .containsExactlyInAnyOrder(CellSnapshotStatus.FLAG, 0);
+    }
+
+    @DisplayName("셀을 열지 않은 경우, 스냅샷의 상태는 UNCHECKED이다.")
+    @Test
+    void getSnapshotWithUncheckedEmptyCell() {
+        // given
+        EmptyCell emptyCell = new EmptyCell();
+
+        // when
+        CellSnapshot snapshot = emptyCell.getSnapshot();
+
+        // then
+        assertThat(snapshot)
+                .extracting("status", "nearbyLandMineCount")
+                .containsExactlyInAnyOrder(CellSnapshotStatus.UNCHECKED, 0);
+    }
 }
