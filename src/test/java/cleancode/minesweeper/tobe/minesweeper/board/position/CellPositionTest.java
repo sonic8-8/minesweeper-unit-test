cbc33e40c93be2567fb13aeaec049022c4e2354c
이승환 <seaung13@gmail.com>
2025-06-24 03:53:33 +0900

    test: [minesweeper] CellPosition.isRowIndexMoreThanOrEqual(), isColIndexMoreThanOrEqual() 경계값 테스트 추가

--- src/test/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositionTest.java
+++ src/test/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositionTest.java
@@ -37,4 +37,56 @@ class CellPositionTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("올바르지 않은 좌표입니다.");
     }
+
+    @DisplayName("셀 위치 정보(CellPosition)의 Row 인덱스가 게임 보드 행 크기(rowSize) 내에 들어오는지 검증한다.")
+    @Test
+    void isRowIndexMoreThanOrEqual() {
+        // given
+        CellPosition cellPosition = CellPosition.of(0, 0);
+
+        // when
+        boolean result = cellPosition.isRowIndexMoreThanOrEqual(0);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("셀 위치 정보(CellPosition)의 Row 인덱스가 게임 보드 행 크기(rowSize)를 초과할 경우 false를 반환한다.")
+    @Test
+    void isRowIndexMoreThanOrEqual_whenOutOfBounds() {
+        // given
+        CellPosition cellPosition = CellPosition.of(0, 0);
+
+        // when
+        boolean result = cellPosition.isRowIndexMoreThanOrEqual(1);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("셀 위치 정보(CellPosition)의 Col 인덱스가 게임 보드 열 크기(colSize) 내에 들어오는지 검증한다.")
+    @Test
+    void isColIndexMoreThanOrEqual() {
+        // given
+        CellPosition cellPosition = CellPosition.of(0, 0);
+
+        // when
+        boolean result = cellPosition.isColIndexMoreThanOrEqual(0);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("셀 위치 정보(CellPosition)의 Cell 인덱스가 게임 보드 열 크기(ColSize)를 초과할 경우 false를 반환한다.")
+    @Test
+    void isColIndexMoreThanOrEqual_whenOutOfBounds() {
+        // given
+        CellPosition cellPosition = CellPosition.of(0, 0);
+
+        // when
+        boolean result = cellPosition.isColIndexMoreThanOrEqual(1);
+
+        // then
+        assertThat(result).isFalse();
+    }
 }
